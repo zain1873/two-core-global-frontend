7ae155d170aa6cf63b98f517461e2c84f9fd3cab
Zain Saleem <zain.saleem155@gmail.com>
2026-07-08 12:13:03 +0500

    updated

--- src/app/pricing/page.jsx
+++
@@ -1,9 +0,0 @@
-import PageComponent from "@/views/pricing";
-import { buildMetadata } from "@/lib/seo";
-
-export const metadata = buildMetadata("/pricing");
-
-export default function Page() {
-  return <PageComponent />;
-}
-
